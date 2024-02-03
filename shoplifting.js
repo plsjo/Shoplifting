8210dc09b3e907a84a5a77ef92d11eae663d78f8
Josias <josiasgv02@gmail.com>
2024-02-03 18:56:42 -0500

    gone

--- shoplifting.js
+++ shoplifting.js
@@ -1,14 +1,3 @@
-const main = document.querySelector('.main');
-main.style.width = '1000px';
-main.style.height = '1000px';
-main.style.backgroundColor = 'black';
-
-const box = document.createElement('div');
-const b = {x:50, y:0, w:40, h:40}
-box.style.backgroundColor = 'green';
-box.style.borderRadius = '50%';
-box.style.width = '${b.w}px'
-box.style.height = '${b.h}px'
-box.style.position = 'relative';
-box.style.left = '${b.x}px';
-ball.style.top = '${b.y}px;';
+function change() {
+    document.getElementById("press").style.display = "none";
+}
