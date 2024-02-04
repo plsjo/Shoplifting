6ceb1df819464ede952f5b8e5892e48ad87d67a8
Josias <josiasgv02@gmail.com>
2024-02-04 00:57:18 -0500

    breading

--- shoplifting.js
+++ shoplifting.js
@@ -1,7 +1,10 @@
 function change() {
     document.getElementById("press").style.display = "none";
+    document.getElementById("pressings").style.display = "none";
 }
 
 function changes() {
     document.getElementById("newish").style.display = "none";
+    document.getElementById("pressed").style.display = "block";
+    document.getElementById("pressing").style.display = "block";
 }
