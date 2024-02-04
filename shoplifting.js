479e428ab1708051f5a4087e814cb1f688174a98
Josias <josiasgv02@gmail.com>
2024-02-04 03:24:31 -0500

    josias was here

--- shoplifting.js
+++ shoplifting.js
@@ -1,4 +1,4 @@
-function changes() {
+/*function changes() {
     document.getElementById("cheese").style.display = "none";
     document.getElementById("tomato").style.display = "block";
     document.getElementById("bread").style.display = "block";
@@ -9,4 +9,11 @@ function changes2() {
     document.getElementById("tomato").style.display = "block";
     document.getElementById("ps5").style.display = "none";
     document.getElementById("bread").style.display = "none";
+}
+*/
+const buttons = ["cheese","bread","tomato","ps5"];
+let visibleButton = [0];
+let hiddenButtons = [1,2,3];
+function changes(buttID) {
+    document.getElementById(buttID).style.display = "none";
 }
